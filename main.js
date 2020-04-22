e1f8cf0c64f42223eb9d86e9c315450f3c94cedf
Kage Zhuang <zhuangfanyin@gmail.com>
2020-04-22 22:22:33 +1000

    add both files

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ new Vue({
     el: "#app",
     data: {
         name: 'kage',
-        email: 'kagezhuang@gmail.com'
+        email: 'kagezhuang@gmail.com',
+        wage: 111,
+        haah: 'feoijfoeiwfeoiefj',
+        super: 3333
     }
 })
